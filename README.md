0f0bb773b0dec4a05d950ce4e28d186261e6af88
DUDU0123 <sdu200115@gmail.com>
2024-11-09 23:23:54 +0530

    Added readme

--- README.md
+++ README.md
@@ -14,3 +14,26 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## App Features
+
+1. User can register using username and password (using jwtToken for authorization).
+2. User can login using username and password (using jwtToken for authorization).
+3. List all books and can view detailed information about the book in description.
+4. List all authors with their name and bio.
+5. User can add ratings to books.
+6. User can search a book.
+7. User can change the theme of the app.
+8. Logout feature.
+
+## Set up the project in your local machine and run
+
+For setting up the project in your local machine and to run the app follow the below steps:
+
+1. Set up flutter in your local system. For that follow the official documentation of  [Flutter](https://docs.flutter.dev/get-started).
+2. Then clone this repository into your local machine.
+3. Open your favourite IDE and open this project in that.
+4. Then run the command in the ide terminal -> flutter pub get.
+5. Then you can run the app in an emulator or physical device by running the following command -> flutter run.
+
+By following this steps you can successfully run the app from your local machine.
--- lib/features/home/data/data_sources/book_data/book_data.dart
+++ lib/features/home/data/data_sources/book_data/book_data.dart
@@ -5,85 +5,101 @@ import 'package:books_app/core/exceptions/exceptions.dart';
 import 'package:books_app/features/home/data/models/book_model.dart';
 import 'package:http/http.dart' as http;
 
+// Abstract interface class defining the contract for book data operations
 abstract interface class BookData {
-  // abstract method for get all books
+  // Abstract method to get all books
   Future<List<BookModel>> getAllBooks();
-  // abstract method for rating add
+
+  // Abstract method to add a rating for a book
   Future<bool> addBookRating({
-    required int starCount,
-    required String bookId,required String jwtToken,
+    required int starCount, // The rating value (e.g., number of stars)
+    required String bookId, // ID of the book to rate
+    required String jwtToken, // JWT token for authorization
   });
 }
 
-// a class that implement all methods of book data abstract class
+// Class that implements all the methods of the BookData abstract class
 class BookDataImpl implements BookData {
-  final http.Client client;
+  final http.Client client; // HTTP client used for making network requests
+
+  // Constructor for injecting the HTTP client dependency
   BookDataImpl({
     required this.client,
   });
-  // implementation for getall books method
+
+  // Implementation of the method to get all books
   @override
   Future<List<BookModel>> getAllBooks() async {
     try {
-      int limit = 10;
+      int limit = 10; // Limit for the number of books fetched
       final response = await client
-          .get(Uri.parse(ApiConstants.allBooks(page: 1, limit: limit)));
+          .get(Uri.parse(ApiConstants.allBooks(page: 1, limit: limit))); // API call to fetch books
+
+      // Check if the response is successful and not empty
       if (response.statusCode == 200 && response.body.isNotEmpty) {
-        final decodedData = jsonDecode(response.body)['result'] as List;
+        final decodedData = jsonDecode(response.body)['result'] as List; // Decode the JSON response
         final List<BookModel> books = decodedData
-            .map((bookJson) => BookModel.fromJson(json: bookJson))
+            .map((bookJson) => BookModel.fromJson(json: bookJson)) // Convert JSON to BookModel objects
             .toList();
-        return books;
+        return books; // Return the list of books
       } else {
-        throw ServerException(message: "No books found");
+        throw ServerException(message: "No books found"); // Throw exception if no books found
       }
     } catch (e) {
-      throw ServerException(message: e.toString());
-    }
-  }
-  // implementation for add book rating method
-  @override
-  Future<bool> addBookRating({
-    required int starCount,
-    required String bookId,required String jwtToken,
-  }) async {
-    try {
-      final response = await client.post(
-        Uri.parse(ApiConstants.addRating(id: bookId)),
-        headers: {
-        'Authorization': 'Bearer $jwtToken', // Add the JWT token in the headers
-      },
-        body: jsonEncode({
-          "rating": starCount.toString(),
-        }),
-      );
-      if (response.statusCode == 200) {
-        log(response.body);
-        return true;
-      } else {
-        log("${response.body}");
-        throw ServerException(message: "Cannot add rating");
-      }
-    } catch (e) {
-      throw ServerException(message: e.toString());
+      throw ServerException(message: e.toString()); // Handle any errors and throw a custom exception
     }
   }
 
-  //get one book by id method
-  Future<BookModel> getOneBook({required String bookId}) async {
+  // Implementation of the method to add a rating for a book
+  @override
+  Future<bool> addBookRating({
+    required int starCount, // The rating value to be added
+    required String bookId, // ID of the book to which the rating is added
+    required String jwtToken, // JWT token for authorization
+  }) async {
     try {
-      final response =
-          await client.get(Uri.parse(ApiConstants.oneBook(bookId: bookId)));
-      log(response.body.toString());
-      if (response.statusCode == 200 && response.body.isNotEmpty) {
-        final decodedData =
-            jsonDecode(response.body)['result'] as Map<String, dynamic>;
-        return BookModel.fromJson(json: decodedData);
+      // API call to add a rating to the specified book
+      final response = await client.post(
+        Uri.parse(ApiConstants.addRating(id: bookId)),
+        headers: {
+          'Authorization': 'Bearer $jwtToken', // Include the JWT token in the request headers
+        },
+        body: jsonEncode({
+          "rating": starCount.toString(), // Encode the star count as a string in the request body
+        }),
+      );
+
+      // Check if the response indicates success
+      if (response.statusCode == 200) {
+        log(response.body); // Log the response body for debugging
+        return true; // Return true if the rating was successfully added
       } else {
-        throw ServerException(message: "Book not found");
+        log("${response.body}"); // Log the response body for debugging if not successful
+        throw ServerException(message: "Cannot add rating"); // Throw exception if the rating couldn't be added
       }
     } catch (e) {
-      throw ServerException(message: e.toString());
+      throw ServerException(message: e.toString()); // Handle any errors and throw a custom exception
+    }
+  }
+
+  // Method to get a single book by its ID
+  Future<BookModel> getOneBook({required String bookId}) async {
+    try {
+      // API call to fetch a single book by its ID
+      final response =
+          await client.get(Uri.parse(ApiConstants.oneBook(bookId: bookId)));
+      log(response.body.toString()); // Log the response body for debugging
+
+      // Check if the response is successful and not empty
+      if (response.statusCode == 200 && response.body.isNotEmpty) {
+        final decodedData =
+            jsonDecode(response.body)['result'] as Map<String, dynamic>; // Decode the JSON response
+        return BookModel.fromJson(json: decodedData); // Convert JSON to a BookModel object
+      } else {
+        throw ServerException(message: "Book not found"); // Throw exception if the book was not found
+      }
+    } catch (e) {
+      throw ServerException(message: e.toString()); // Handle any errors and throw a custom exception
     }
   }
 }
